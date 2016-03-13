a2715fb252598b94727c7544f3dd252fa593d74b
do0g <do0g@users.noreply.github.com>
2016-03-13 12:40:25 +0000

    Refactor and clean up

--- src/node-deep.js
+++ src/node-deep.js
@@ -3,26 +3,34 @@ import { curry,
          toPairs,
          map,
          is,
-         concat,
+         append,
          last,
          head,
          reduce,
-         not } from 'ramda';
+         not,
+         mergeAll,
+         binary } from 'ramda';
 
-function nodeDeep(obj, callback, path = []) {
-  if (is(Object, obj) && !is(Date, obj) && !is(RegExp, obj) && !is(Function, obj)) {
-    return fromPairs(map(curry(iteratee)(callback, path), toPairs(obj)));
+const isPlainObject = obj => is(Object, obj) && !is(Date, obj) && !is(RegExp, obj) && !is(Function, obj);
+
+function _deep(callback, obj, path = []) {
+  if (isPlainObject(obj)) {
+    return mergeAll(map(iteratee(callback, path), toPairs(obj)));
   }
   return obj;
 }
 
-function iteratee(callback, path, value) {
-  const p = concat(path, head(value));
-  const l = last(value);
-  if (not(callback(p, l))) {
-    const res = nodeDeep(l, callback, p);
-    return [head(value), res];
+const iteratee = curry((callback, path, pair) => {
+  const newPath = append(head(pair), path);
+  const obj     = last(pair);
+  const res     = callback(newPath, obj);
+  if (isPlainObject(res)) {
+    return res;
+  } else if (!res) {
+    return { [head(pair)]: _deep(callback, obj, newPath) };
   }
-}
+});
 
-export default nodeDeep;
+const deep = binary(_deep);
+
+export default deep;
--- test/unit/node-deep.js
+++ test/unit/node-deep.js
@@ -1,9 +1,9 @@
 import deep from '../../src/node-deep';
 
-describe('nodeDeep', () => {
-  it('should call back for every node', () => {
+describe('deep', () => {
+  it('calls back for every node', () => {
     const cb = spy();
-    deep({ a: 'b', c: { d: { e: 'f' } } }, cb);
+    deep(cb, { a: 'b', c: { d: { e: 'f' } } });
     expect(cb).to.have.been.calledWith(['a'], 'b');
     expect(cb).to.have.been.calledWith(['c'], { d: { e: 'f' } });
     expect(cb).to.have.been.calledWith(['c', 'd'], { e: 'f' });
@@ -13,14 +13,24 @@ describe('nodeDeep', () => {
   it(`doesn't mutate the source object graph`, () => {
     const pred = () => false;
     const obj = { a: 'b' };
-    const res = deep(obj, pred);
+    const res = deep(pred, obj);
     expect(res).not.to.equal(obj);
     expect(res).to.deep.equal(obj);
   });
 
   it('omits', () => {
     const pred = (path, val) => path.join('.') === 'c.d';
-    const res = deep({ a: 'b', c: { d: { e: 'f' } } }, pred);
+    const res = deep(pred, { a: 'b', c: { d: { e: 'f' } } });
     expect(res).to.eql({ a: 'b', c: {} });
+  });
+
+  it('replaces', () => {
+    const pred = (path, val) => {
+      if (path.join('.') === 'c.d.e') {
+        return { f: 'g' };
+      };
+    };
+    const res = deep(pred, { a: 'b', c: { d: { e: 'f' } } });
+    expect(res).to.eql({ a: 'b', c: { d: { f: 'g' } } });
   });
 });
