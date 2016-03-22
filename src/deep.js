5675e57a146b3c1fa8fc8b4fff28a704d76b1513
Doug Hardy <do0g@users.noreply.github.com>
2016-03-22 12:20:13 +0000

    Remove _deep function

--- src/deep.js
+++ src/deep.js
@@ -18,9 +18,9 @@ import { curry,
 
 const isPlainObject = obj => is(Object, obj) && !is(Date, obj) && !is(RegExp, obj) && !is(Function, obj);
 
-function _deep(callback, obj, path) {
+function deep(callback, obj, path) {
   if (is(Array, obj)) {
-    return filter(compose(not, isEmpty), map(curry(_deep)(callback, __, path), obj));
+    return filter(compose(not, isEmpty), map(curry(deep)(callback, __, path), obj));
   }
   if (isPlainObject(obj)) {
     return mergeAll(map(iteratee(callback, path), toPairs(obj)));
@@ -35,10 +35,8 @@ const iteratee = curry((callback, path, pair) => {
   if (isPlainObject(res)) {
     return res;
   } else if (!res) {
-    return { [head(pair)]: _deep(callback, obj, newPath) };
+    return { [head(pair)]: deep(callback, obj, newPath) };
   }
 });
 
-const deep = (callback, obj) => _deep(callback, obj, []);
-
-export default deep;
+export default (callback, obj) => deep(callback, obj, []);
